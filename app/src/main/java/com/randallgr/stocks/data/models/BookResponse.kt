165fcadcfe563fddf92898855cc61e38616e7f2c
Randall16 <randallgr5@outlook.com>
2018-12-29 13:23:22 -0500

    Added documentation

--- app/src/main/java/com/randallgr/stocks/data/models/BookResponse.kt
+++ app/src/main/java/com/randallgr/stocks/data/models/BookResponse.kt
@@ -1,17 +1,24 @@
+/**
+ * The IEX API gives a bloated response when pulling Quote pricing. Alls that matters is the Quote JSON object
+ * but it is nested inside of JSON object so this BookResponse class is here to hold that outer class. The data we
+ * care about can be found in the Quote data class. The properties asks, bids, and systemEvent will likely be null.
+ */
+
 package com.randallgr.stocks.data.models
 
 import com.google.gson.annotations.SerializedName
 
+
 data class BookResponse(
     @SerializedName("asks")
-    val asks: List<Any>?,
+    private val asks: List<Any>?,
 
     @SerializedName("bids")
-    val bids: List<Any>?,
+    private val bids: List<Any>?,
 
-    @SerializedName("quote")
+    @SerializedName("quote")    // This property is the one holding all the relevant data
     val quote: Quote,
 
     @SerializedName("systemEvent")
-    val systemEvent: Any?
+    private  val systemEvent: Any?
 )
